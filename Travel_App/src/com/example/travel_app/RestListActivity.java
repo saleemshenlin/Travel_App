030e6a9bda934542cc76a65e9445bf62af7c36f8
沈霖 <saleemshenlin@gmail.com>
2014-01-13 08:42:10 +0800

    SlashActivity & HomeActivity

---
+++ Travel_App/src/com/example/travel_app/RestListActivity.java
@@ -0,0 +1,12 @@
+package com.example.travel_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RestListActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
