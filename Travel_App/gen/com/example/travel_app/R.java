ce6e9e5dc45f6fc4b52df740fa8cf4cf6a0600d5
沈霖 <saleemshenlin@gmail.com>
2014-01-16 10:56:16 +0800

    添加 ArcGIS SDK

--- Travel_App/gen/com/example/travel_app/R.java
+++ Travel_App/gen/com/example/travel_app/R.java
@@ -60,8 +60,9 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
-        public static final int activity_sceniclist=0x7f030001;
-        public static final int activity_splash=0x7f030002;
+        public static final int activity_map=0x7f030001;
+        public static final int activity_sceniclist=0x7f030002;
+        public static final int activity_splash=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f080000;
