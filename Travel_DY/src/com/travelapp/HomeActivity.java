fa246539126e1a2e41ab21c1a82cd14c5aa4bbb2
沈霖 <saleemshenlin@gmail.com>
2014-01-29 16:19:33 +0800

    add WXapi & Weiboapi

--- Travel_DY/src/com/travelapp/HomeActivity.java
+++ Travel_DY/src/com/travelapp/HomeActivity.java
@@ -1,10 +1,20 @@
 package com.travelapp;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.SendMessageToWX;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+import com.tencent.mm.sdk.openapi.WXMediaMessage;
+import com.tencent.mm.sdk.openapi.WXTextObject;
 
 public class HomeActivity extends Activity {
 
@@ -14,12 +24,15 @@ public class HomeActivity extends Activity {
 	private ImageView mRestImageView;
 	private ImageView mFunImageView;
 	private ImageView mRouteImageView;
+	private ImageView mSocialImageView;
+	private IWXAPI mIwxapi;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
 		initView();
+		initWX();
 	}
 
 	private void initView() {
@@ -29,6 +42,7 @@ public class HomeActivity extends Activity {
 		mFunImageView = (ImageView) findViewById(R.id.imgItemFun);
 		mRestImageView = (ImageView) findViewById(R.id.imgItemRest);
 		mRouteImageView = (ImageView) findViewById(R.id.imgItemRoute);
+		mSocialImageView = (ImageView) findViewById(R.id.imgItemSocial);
 		mMapImageView.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
@@ -100,6 +114,61 @@ public class HomeActivity extends Activity {
 						R.anim.anim_in_right2left, R.anim.anim_out_right2left);
 			}
 		});
+		mSocialImageView.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				final EditText editor = new EditText(HomeActivity.this);
+				editor.setLayoutParams(new LinearLayout.LayoutParams(
+						LinearLayout.LayoutParams.FILL_PARENT,
+						LinearLayout.LayoutParams.WRAP_CONTENT));
+				editor.setText(R.string.app_name);
+				String text = "测试";
+
+				// 初始化一个WXTextObject对象
+				WXTextObject textObj = new WXTextObject();
+				textObj.text = text;
+
+				// 用WXTextObject对象初始化一个WXMediaMessage对象
+				WXMediaMessage msg = new WXMediaMessage();
+				msg.mediaObject = textObj;
+				// 发送文本类型的消息时，title字段不起作用
+				// msg.title = "Will be ignored";
+				msg.description = text;
+
+				// 构造一个Req
+				SendMessageToWX.Req req = new SendMessageToWX.Req();
+				req.transaction = buildTransaction("text"); // transaction字段用于唯一标识一个请求
+				req.message = msg;
+				// req.scene = isTimelineCb.isChecked() ?
+				// SendMessageToWX.Req.WXSceneTimeline
+				// : SendMessageToWX.Req.WXSceneSession;
+				req.scene = SendMessageToWX.Req.WXSceneTimeline;
+				// 调用api接口发送数据到微信
+				boolean isSend = mIwxapi.sendReq(req);
+				if (isSend) {
+					Log.e("WX", "OK");
+				} else {
+					Log.e("WX", "Error");
+				}
+			}
+		});
 	}
 
+	public void initWX() {
+		mIwxapi = WXAPIFactory.createWXAPI(HomeActivity.this,
+				HomeActivity.this.getString(R.string.wx_app_id), false);
+		boolean isRegist = mIwxapi.registerApp(HomeActivity.this
+				.getString(R.string.wx_app_id));
+		if (isRegist) {
+			Log.e("WX", "OK");
+		} else {
+			Log.e("WX", "Error");
+		}
+	}
+
+	private String buildTransaction(final String type) {
+		return (type == null) ? String.valueOf(System.currentTimeMillis())
+				: type + System.currentTimeMillis();
+	}
 }
